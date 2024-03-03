7e31055393d6a89767c75fdb44f41b80d7ec0fd6
nurmurodoveec <137428953+nurmurodoveec@users.noreply.github.com>
2024-03-03 17:18:30 +0500

    STEP8

--- Project/UsersDbContext.cs
+++ Project/UsersDbContext.cs
@@ -12,6 +12,7 @@ namespace Project
         public virtual DbSet<Users> Users { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
         {
+
             optionBuilder.UseSqlServer("Data Source=WIN-68JTTU6BA4D\\MSSQLSERVER01;Initial Catalog=Users1;Integrated Security=True;Pooling=False;Encrypt=False;Trust Server Certificate=False");
             base.OnConfiguring(optionBuilder);
         }
