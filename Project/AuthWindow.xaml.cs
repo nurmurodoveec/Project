656d40d39747cf9535117818026ec89d204ca902
nurmurodoveec <137428953+nurmurodoveec@users.noreply.github.com>
2024-03-04 18:29:45 +0500

    фыфыф

--- Project/AuthWindow.xaml.cs
+++ Project/AuthWindow.xaml.cs
@@ -7,8 +7,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Forms;
-
+using System.Windows.Forms
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -40,6 +39,7 @@ namespace Project
                 passBox1.Background = Brushes.LightPink;
             }
             else
+
             {
                 textBoxLogin1.ToolTip = "";
                 textBoxLogin1.Background = Brushes.Transparent;
