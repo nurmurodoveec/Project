f440ea55954f756aff76279c7d975aec15a9b829
nurmurodoveec <137428953+nurmurodoveec@users.noreply.github.com>
2024-03-10 16:45:28 +0500

    10.03

--- Project/AuthWindow.xaml.cs
+++ Project/AuthWindow.xaml.cs
@@ -33,7 +33,6 @@ namespace Project
                 textBoxLogin1.ToolTip = "This field was entered incorrectly";
                 textBoxLogin1.Background = Brushes.LightPink;
             }
-
             else if (pass.Length < 5)
             {
                 passBox1.ToolTip = "This field was entered incorrectly";
